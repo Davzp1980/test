b69792b2b32c8bcf9c2c4bf442c0c5e12c1ac20a
Oleksii Donets <davzp1980@gmail.com>
2022-08-11 15:45:03 +0300

    fix errors

--- test.go
+++ test.go
@@ -2,16 +2,15 @@ package main
 
 import (
 	"fmt"
-	"math"
 )
 
 func main() {
 
 	x := 9
 	y := 3
-    z :=5
-	r := math.Sqrt(float64(x)*float64(y) + float64(x)/float64(y))
+	z := 5
+	r := x + y
 	fmt.Println(int(r))
-    fmt.Println(z)
+	fmt.Println(z)
 
 }
